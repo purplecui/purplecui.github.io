71f0cb5ab38c88e5f835b31b9f577bbd2d37a727
purplecui <gauravkhadka4593@gmail.com>
2025-02-02 16:46:47 +0545

    Quartz sync: Feb 2, 2025, 4:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "",
+    pageTitle: "🍁 G-WRITES",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
